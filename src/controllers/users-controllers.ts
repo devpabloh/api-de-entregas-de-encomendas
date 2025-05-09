7ec68c83cfffa1fc0b44d81712a61f26e55c489a
Pablo Henrique <pablohenriqueadm94@gmail.com>
2025-05-09 11:46:35 -0300

    feat: update Prisma and Zod dependencies, enhance user controller validation
    - Updated Prisma and Zod to their latest versions for improved functionality and bug fixes.
    - Enhanced user controller with Zod schema validation for user creation.
    - Added new Prisma schema fields and relationships for deliveries and logs.

--- src/controllers/users-controllers.ts
+++ src/controllers/users-controllers.ts
@@ -1,8 +1,18 @@
 import { Request, Response } from "express";
+import {z} from "zod"
 
 class UsersControllers{
 
     create(request: Request, response: Response){
+        
+        const bodySchema = z.object({
+            name: z.string(),
+            email: z.string().email(),
+            password: z.string().min(6)
+        })
+
+        const {email, name, password} = bodySchema.parse(request.body)
+
         return response.json({message: "User created"})
     }
 }
