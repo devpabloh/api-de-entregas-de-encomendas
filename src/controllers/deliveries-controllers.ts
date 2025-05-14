42910e22d38e68692234c76a1afb82fc2057ec8f
Pablo Henrique <pablohenriqueadm94@gmail.com>
2025-05-14 11:22:02 -0300

    feat: adicionar middleware de autorização de usuário e controlador de status de entrega
    Este commit introduz um novo middleware `verifyUserAuthorization` para aplicar o controle de acesso baseado em funções. Além disso, adiciona um `DeliveriesStatusController` para lidar com atualizações de status de entrega. As rotas de entrega foram atualizadas para incluir o novo middleware e controlador, garantindo que apenas usuários autorizados possam executar determinadas ações.

--- src/controllers/deliveries-controllers.ts
+++ src/controllers/deliveries-controllers.ts
@@ -1,9 +1,39 @@
 import {Request, Response} from "express"
+import {prisma} from "@/database/prisma"
+import {z} from "zod"
 
 class DeliveriesControllers {
-    create (request: Request, response: Response){
-        return response.json({message: "O pedido foi confirmado"})
+    async create(request: Request, response: Response){
+
+        const bodySchema = z.object({
+            user_id: z.string().uuid(), // user_id é o id do usuário que está fazendo o pedido
+            description: z.string(),
+        })
+        
+        const {user_id, description} = bodySchema.parse(request.body)
+
+        await prisma.delivery.create({
+            data: {
+                userId: user_id,
+                description: description
+            }
+        })
+        
+        return response.status(201).json()
     }
+
+    async index(request: Request, response: Response){
+
+        const deliveries = await prisma.delivery.findMany({
+            include: {
+                user: {select: {name: true, email: true}}
+            }
+        })
+
+        return response.json(deliveries)
+    }
+
+
 }
 
 export {DeliveriesControllers}
--- src/routes/deliveries-routes.ts
+++ src/routes/deliveries-routes.ts
@@ -1,14 +1,21 @@
 import { Router } from "express";
 
 import { DeliveriesControllers } from "@/controllers/deliveries-controllers";
+import { DeliveriesStatusController } from "@/controllers/deliveries-status-controllers";
 
 import { ensureAuthenticated } from "@/middlewares/ensure-authenticated";
 
+import { verifyUserAuthorization } from "@/middlewares/verifyUserAuthorization";
+
 const routerDeliveries = Router()
 const deliveriesControllers = new DeliveriesControllers()
+const deliveriesStatusController = new DeliveriesStatusController()
 
-routerDeliveries.use(ensureAuthenticated)
+routerDeliveries.use(ensureAuthenticated, verifyUserAuthorization(["admin"]))
 
 routerDeliveries.post("/", deliveriesControllers.create)
+routerDeliveries.get("/", deliveriesControllers.index)
+
+routerDeliveries.patch("/:id/status", deliveriesStatusController.updated)
 
 export {routerDeliveries}
