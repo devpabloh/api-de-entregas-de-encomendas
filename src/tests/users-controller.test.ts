84158cd8b7813c691e09d75d9d6e6bb626cf56aa
Pablo Henrique <pablohenriqueadm94@gmail.com>
2025-05-16 13:09:05 -0300

    test(users-controller): add cleanup and additional test cases
    Add cleanup logic to delete test user after all tests are completed. Include additional test cases to verify error handling for duplicate emails and invalid email validation.

--- src/tests/users-controller.test.ts
+++ src/tests/users-controller.test.ts
@@ -1,7 +1,14 @@
 import request from "supertest"
 import { app } from "@/app"
+import {prisma} from "@/database/prisma"
 
 describe('UsersController', () => {
+    let userId: string
+
+    afterAll( async ()=>{
+        await prisma.user.delete({where: {id: userId}})
+    })
+
     it("should create a new user sucessfully", async () => {
         const response = await request(app).post("/users").send({
             name: "Test user",
@@ -12,5 +19,26 @@ describe('UsersController', () => {
         expect(response.status).toBe(201)
         expect(response.body).toHaveProperty("id")
         expect(response.body.name).toBe("Test user")
+
+        userId = response.body.id
+    })
+
+    it("should throw an error if user with same email already exists", async ()=>{
+        const response = await request(app).post("/users").send({
+            name: "duplicate user",
+            email: "testeuser@example.com",
+            password: "123456"
+        })
+
+        expect(response.status).toBe(400)
+        expect(response.body.message).toBe("User with same email already exists")
+    })
+
+    it("should throw a validation error if email is invalid", async ()=>{
+        const response = await request(app).post("/users").send(
+            {
+                
+            }
+        )
     })
 })
